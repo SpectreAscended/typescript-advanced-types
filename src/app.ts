91738ebe3835288a0cb6b3b7aad048e2ee6361b6
Cory Pollard <astandard87@gmail.com>
2023-02-27 11:56:40 -0600

    initial file upload

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+// Intersection types
+
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+// interface Admin {
+//   name: string;
+//   privileges: string[];
+// }
+
+// interface Employee {
+//   name: string;
+//   startDate: Date;
+// }
+
+// interface ElevatedEmployee extends Admin, Employee {}
+
+// ^ These basically do the same thing
+
+const e1: ElevatedEmployee = {
+  name: 'Cory',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
